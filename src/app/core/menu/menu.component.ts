44907e26220492c33a122cef84b23c61d94432f6
Sean Blessing <snblessing@gmail.com>
2024-01-24 10:18:23 -0500

    init commit

---
+++ src/app/core/menu/menu.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'src/app/model/menu-item';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  title: string = "Menu Component";
+  menuItems: MenuItem[] = [];
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+    this.menuItems = [
+      new MenuItem("Product", "/product/list", "Product List")
+      //new MenuItem("Line Item List", "/li/list", "Line Item List")
+    ];
+  }
+}
