29ec32a21e908bdb5c217dc2a36401db564b733d
Sean Blessing <snblessing@gmail.com>
2024-01-25 10:49:47 -0500

    li create and edit components done

--- src/app/feature/line-item/line-item-create/line-item-create.component.ts
+++ src/app/feature/line-item/line-item-create/line-item-create.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LineItem } from 'src/app/model/line-item';
+import { Product } from 'src/app/model/product';
+import { LineItemService } from 'src/app/service/line-item.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-line-item-create',
   templateUrl: './line-item-create.component.html',
   styleUrls: ['./line-item-create.component.css']
 })
-export class LineItemCreateComponent {
+export class LineItemCreateComponent implements OnInit, OnDestroy {
+  title: string = 'Line-Item-Create';
+  newLineItem: LineItem = new LineItem();
+  subscription: Subscription | undefined;
+  message?: string = undefined;
+  products?: Product[] = undefined; 
 
+  constructor(
+    private lineItemSvc: LineItemService,
+    private productSvc: ProductService,
+    private router: Router
+  ) {}
+  
+  ngOnInit(): void {
+    // load list of products for drop down
+    this.subscription = this.productSvc.list().subscribe(
+      resp => {
+        this.products = resp;
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  save() {
+    // Make an assumption that the user will NOT add the same product +
+    // invoice combo that already exists in the DB.
+    this.subscription = this.lineItemSvc.create(this.newLineItem).subscribe(
+      resp => {
+        this.newLineItem = resp;
+        this.router.navigateByUrl("/line-item/list");
+      }
+    );
+  }
 }
--- src/app/feature/line-item/line-item-edit/line-item-edit.component.ts
+++ src/app/feature/line-item/line-item-edit/line-item-edit.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LineItem } from 'src/app/model/line-item';
+import { Product } from 'src/app/model/product';
+import { LineItemService } from 'src/app/service/line-item.service';
+import { ProductService } from 'src/app/service/product.service';
 
 @Component({
   selector: 'app-line-item-edit',
   templateUrl: './line-item-edit.component.html',
   styleUrls: ['./line-item-edit.component.css']
 })
-export class LineItemEditComponent {
+export class LineItemEditComponent implements OnInit, OnDestroy{
+  title: string = 'Line-Item-Edit';
+  lineItem: LineItem = new LineItem();
+  // the id of the line item we want to edit
+  liId: number = 0;
+  subscription: Subscription | undefined;
+  message?: string = undefined;
+  products?: Product[] = undefined; 
 
+  constructor(
+    private lineItemSvc: LineItemService,
+    private productSvc: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.productSvc.list().subscribe(
+      resp => {
+        this.products = resp;
+        this.subscription?.unsubscribe();
+        // get the line item id from the route
+        this.subscription = this.route.params.subscribe(
+          parms => {
+            this.liId = parms["id"];
+            this.subscription?.unsubscribe();
+            // get the line item for liId
+            if (this.liId) {
+              this.subscription = this.lineItemSvc.get(this.liId).subscribe(
+                resp => {
+                  this.lineItem = resp;
+                  this.subscription?.unsubscribe();
+                }
+              );
+            }
+            else {
+              console.log("Error: no ide passed for lineItem.");
+            }
+          }
+        );
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  save() {
+    this.subscription = this.lineItemSvc.edit(this.lineItem).subscribe(
+      resp => {
+        this.lineItem = resp;
+        this.router.navigateByUrl("/line-item/list");
+      }
+    );
+   }
+
+   compProduct(a: Product, b: Product): boolean {
+    return a && b && a.productCode === b.productCode;
+  }
 }
